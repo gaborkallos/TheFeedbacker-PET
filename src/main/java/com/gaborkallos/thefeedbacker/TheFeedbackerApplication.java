521353c4a92803322b3a8b4004e3769543bf2dce
Akos Kolloros <kollorosa@gmail.com>
2019-06-04 10:50:36 +0200

    Swagger available on "http://localhost:5000/swagger-ui.html"

--- src/main/java/com/gaborkallos/thefeedbacker/TheFeedbackerApplication.java
+++ src/main/java/com/gaborkallos/thefeedbacker/TheFeedbackerApplication.java
@@ -4,8 +4,15 @@ import com.gaborkallos.thefeedbacker.service.SystemAdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class TheFeedbackerApplication {
 
     private SystemAdminService systemAdminService;
@@ -18,8 +25,16 @@ public class TheFeedbackerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TheFeedbackerApplication.class, args);
+    }
 
-
+    @Bean
+    public Docket swaggerApi() {
+        // http://localhost:5000/swagger-ui.html
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 
 }
