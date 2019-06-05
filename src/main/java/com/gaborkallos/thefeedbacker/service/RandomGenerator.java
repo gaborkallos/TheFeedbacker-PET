ef27dcbb95b388602d715d833e2461c270e0e5da
gaborkallos <gaborkallos@gmail.com>
2019-06-05 15:10:45 +0200

    randomize password

--- src/main/java/com/gaborkallos/thefeedbacker/service/RandomGenerator.java
+++ src/main/java/com/gaborkallos/thefeedbacker/service/RandomGenerator.java
@@ -7,13 +7,18 @@ public class RandomGenerator {
     private Random RANDOM = new Random();
 
     public String passwordGenerator() {
-        String abc = "abcdefghijklmnopqrstuvxyz";
+        String abc = "abcdefghijklmnopqrstuvwxyz";
+        String numbers = "1234567890";
         StringBuilder password = new StringBuilder();
         for (int i = 0; i < 10; i++) {
-            if (RANDOM.nextBoolean()) {
-                password.append(abc.charAt(RANDOM.nextInt(abc.length())));
+            if (i == 5 || i == 6) {
+                password.append(numbers.charAt(RANDOM.nextInt(numbers.length())));
             } else {
-                password.append(Character.toUpperCase(abc.charAt(RANDOM.nextInt(abc.length()))));
+                if (RANDOM.nextBoolean()) {
+                    password.append(abc.charAt(RANDOM.nextInt(abc.length())));
+                } else {
+                    password.append(Character.toUpperCase(abc.charAt(RANDOM.nextInt(abc.length()))));
+                }
             }
         }
         return password.toString();
