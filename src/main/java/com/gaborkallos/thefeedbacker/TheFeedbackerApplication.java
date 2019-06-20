6067f800f8fdea8503ea0e3d77c421920a3adda9
Akos Kolloros <kollorosa@gmail.com>
2019-06-20 09:02:10 +0200

    Country and Cities initial values on startup

--- src/main/java/com/gaborkallos/thefeedbacker/TheFeedbackerApplication.java
+++ src/main/java/com/gaborkallos/thefeedbacker/TheFeedbackerApplication.java
@@ -1,7 +1,12 @@
 package com.gaborkallos.thefeedbacker;
 
+import com.gaborkallos.thefeedbacker.model.City;
+import com.gaborkallos.thefeedbacker.model.Country;
+import com.gaborkallos.thefeedbacker.repository.CityRepository;
+import com.gaborkallos.thefeedbacker.repository.CountryRepository;
 import com.gaborkallos.thefeedbacker.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,11 +16,18 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 @EnableSwagger2
 public class TheFeedbackerApplication {
 
     private AdminService adminService;
+
+    @Autowired
+    private CityRepository cityRepository;
+    @Autowired
+    private CountryRepository countryRepository;
 
     @Autowired
     public void setAdminService(AdminService adminService) {
@@ -35,6 +47,22 @@ public class TheFeedbackerApplication {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    @Bean
+    public CommandLineRunner init() {
+        return args -> {
+            City city1 = new City(1L, "Budapest");
+            City city2 = new City(2L, "London");
+            City city3 = new City(3L, "New York");
+
+            Country country1 = new Country(1L, "Hungary");
+            Country country2 = new Country(2L, "United Kingdom");
+            Country country3 = new Country(3L, "United States");
+
+            cityRepository.saveAll(Arrays.asList(city1, city2, city3));
+            countryRepository.saveAll(Arrays.asList(country1, country2, country3));
+        };
     }
 
 }
